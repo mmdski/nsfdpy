59fd88c0cedb59ae4622e586a6d129aa11cbc198
Marian Domanski <mmdski@gmail.com>
2024-02-11 20:58:32 -0600

    implement PGrid, UGrid, and VGrid

--- nsfd/grid.py
+++ nsfd/grid.py
@@ -8,10 +8,9 @@ import nsfd.libnsfd as libnsfd
 
 
 class Grid:
+    """Grid base class"""
 
-    def __init__(
-        self, m_rows, n_cols, x_min=0, x_max=1, y_min=0, y_max=1, init_value=0
-    ):
+    def __init__(self, m_rows, n_cols, x_min, x_max, y_min, y_max, init_value=0):
 
         if m_rows < 0:
             raise ValueError
@@ -65,12 +64,7 @@ class Grid:
         if ax is None:
             ax = plt.axes()
 
-        ax.scatter(
-            self._x_coords.flatten(),
-            self._y_coords.flatten(),
-            c=self.values().flatten(),
-            **kwargs
-        )
+        ax.scatter(self._x_coords.flatten(), self._y_coords.flatten(), **kwargs)
 
         return ax
 
@@ -80,3 +74,99 @@ class Grid:
         libnsfd.grid_get_values(self._grid_ptr, values.ctypes.data_as(libnsfd.Real_p))
 
         return values
+
+
+class PGrid(Grid):
+    """Pressure grid"""
+
+    def __init__(self, grid_props):
+
+        m_cell_rows = grid_props["m_rows"]
+        n_cell_cols = grid_props["n_cols"]
+
+        domain_x_min = grid_props["x_min"]
+        domain_x_max = grid_props["x_max"]
+        dx = (domain_x_max - domain_x_min) / n_cell_cols
+
+        grid_x_min = domain_x_min + dx / 2
+        grid_x_max = domain_x_max - dx / 2
+
+        domain_y_min = grid_props["y_min"]
+        domain_y_max = grid_props["y_max"]
+        dy = (domain_y_max - domain_y_min) / m_cell_rows
+
+        grid_y_min = domain_y_min + dy / 2
+        grid_y_max = domain_y_max - dy / 2
+
+        super().__init__(
+            m_cell_rows,
+            n_cell_cols,
+            grid_x_min,
+            grid_x_max,
+            grid_y_min,
+            grid_y_max,
+        )
+
+
+class UGrid(Grid):
+    """Horizontal (x-axis) velocity grid"""
+
+    def __init__(self, grid_props):
+
+        m_rows = grid_props["m_rows"]
+        n_cols = grid_props["n_cols"] + 1
+
+        domain_x_min = grid_props["x_min"]
+        domain_x_max = grid_props["x_max"]
+
+        # include left boundary nodes
+        grid_x_min = domain_x_min
+        grid_x_max = domain_x_max
+
+        domain_y_min = grid_props["y_min"]
+        domain_y_max = grid_props["y_max"]
+        dy = (domain_y_max - domain_y_min) / m_rows
+
+        grid_y_min = domain_y_min + dy / 2
+        grid_y_max = domain_y_max - dy / 2
+
+        super().__init__(
+            m_rows,
+            n_cols,
+            grid_x_min,
+            grid_x_max,
+            grid_y_min,
+            grid_y_max,
+        )
+
+
+class VGrid(Grid):
+    """Vertical (y-axis) velocity grid"""
+
+    def __init__(self, grid_props):
+
+        m_rows = grid_props["m_rows"] + 1
+        n_cols = grid_props["n_cols"]
+
+        domain_x_min = grid_props["x_min"]
+        domain_x_max = grid_props["x_max"]
+        dx = (domain_x_max - domain_x_min) / n_cols
+
+        grid_x_min = domain_x_min + dx / 2
+        grid_x_max = domain_x_max - dx / 2
+
+        domain_y_min = grid_props["y_min"]
+        domain_y_max = grid_props["y_max"]
+
+        # include bottom boundary nodes
+        grid_y_min = domain_y_min
+        grid_y_max = domain_y_max
+
+        super().__init__(
+            m_rows,
+            n_cols,
+            grid_x_min,
+            grid_x_max,
+            grid_y_min,
+            grid_y_max,
+        )
