93a1a60ecd017af88f24cf785dd28186d341c4d0
Marian Domanski <mmdski@gmail.com>
2024-12-16 21:07:04 -0600

    move bindings to individual source files

---
+++ src/nsfdpy/field/bind_scalar.cpp
@@ -0,0 +1,95 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+
+#include <nsfd/field/scalar.hpp>
+
+namespace py = pybind11;
+
+namespace {
+py::module np = py::module::import("numpy");
+
+nsfd::Scalar __getitem__(nsfd::field::Scalar &self,
+                         std::tuple<size_t, size_t> idx) {
+  return self(std::get<0>(idx), std::get<1>(idx));
+}
+
+void __setitem__(nsfd::field::Scalar &self, std::tuple<size_t, size_t> idx,
+                 double s) {
+  self(std::get<0>(idx), std::get<1>(idx)) = nsfd::Scalar(s);
+}
+
+nsfd::field::Scalar *loadtxt(const std::string &filename) {
+  py::object dtype = np.attr("float64");
+  py::array s = np.attr("loadtxt")(py::str(filename), py::arg("dtype") = dtype,
+                                   py::arg("ndmin") = 2);
+  const ssize_t *shape = s.shape();
+  if (shape[0] < 3)
+    throw std::runtime_error("shape[0] must be greater than or equal to 3");
+  if (shape[1] < 3)
+    throw std::runtime_error("shape[1] must be greater than or equal to 3");
+
+  size_t n_i = static_cast<size_t>(shape[0]);
+  size_t n_j = static_cast<size_t>(shape[1]);
+
+  nsfd::field::Scalar *sf = new nsfd::field::Scalar(n_i - 2, n_j - 2);
+  const double *s_data = static_cast<const double *>(s.data());
+
+  for (size_t i = 0; i < n_i; ++i) {
+    for (size_t j = 0; j < n_j; ++j) {
+      sf->operator()(i, j) = s_data[i * n_j + j];
+    }
+  }
+
+  return sf;
+}
+
+void savetxt(nsfd::field::Scalar &self, const std::string &filename) {
+  auto [n_i, n_j] = self.shape();
+  py::array_t<double> s({n_i, n_j});
+  auto s_buf = s.mutable_unchecked<2>();
+
+  for (size_t i = 0; i < n_i; ++i) {
+    for (size_t j = 0; j < n_j; ++j) {
+      s_buf(i, j) = static_cast<double>(self(i, j));
+    }
+  }
+
+  np.attr("savetxt")(filename, s);
+}
+
+py::array_t<double> values(nsfd::field::Scalar &self) {
+  auto [n_i, n_j] = self.shape();
+  py::array_t<double> s({n_i, n_j});
+  auto s_buf = s.mutable_unchecked<2>();
+
+  for (size_t i = 0; i < n_i; ++i) {
+    for (size_t j = 0; j < n_j; ++j) {
+      s_buf(i, j) = static_cast<double>(self(i, j));
+    }
+  }
+  return s;
+}
+}  // namespace
+
+namespace nsfdpy {
+namespace field {
+void bindScalar(py::module_ &m) {
+  py::class_<nsfd::field::Scalar>(m, "ScalarField")
+      .def(py::init<size_t, size_t>())
+      .def(py::init<size_t, size_t, double>())
+      .def(py::init<size_t, size_t, nsfd::Scalar>())
+      .def("__getitem__", &__getitem__)
+      .def("__setitem__", &__setitem__)
+      .def_static("loadtxt", &loadtxt)
+      .def("max_abs", &nsfd::field::Scalar::max_abs)
+      .def("savetxt", &savetxt)
+      .def("shape", &nsfd::field::Scalar::shape)
+      .def_property_readonly("values", &values);
+}
+}  // namespace field
+}  // namespace nsfdpy
---
+++ src/nsfdpy/grid/bind_grid.cpp
@@ -0,0 +1,52 @@
+/*
+ * This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at https://mozilla.org/MPL/2.0/.
+ */
+#include <pybind11/numpy.h>
+#include <pybind11/pybind11.h>
+
+#include <nsfd/grid/grid.hpp>
+
+namespace py = pybind11;
+
+namespace {
+
+py::module np = py::module::import("numpy");
+
+nsfd::grid::Grid *__init__(const py::dict &geom_data, double xfrac = 1,
+                           double yfrac = 1) {
+  return new nsfd::grid::Grid(geom_data["xlength"].cast<double>(),
+                              geom_data["imax"].cast<size_t>(),
+                              geom_data["ylength"].cast<double>(),
+                              geom_data["jmax"].cast<size_t>(), xfrac, yfrac);
+}
+
+py::tuple meshgrid(const nsfd::grid::Grid &self) {
+  py::array_t<double> X({self.x.max + 2, self.y.max + 2});
+  py::array_t<double> Y({self.x.max + 2, self.y.max + 2});
+  auto X_buf = X.mutable_unchecked<2>();
+  auto Y_buf = Y.mutable_unchecked<2>();
+  for (size_t i = 0; i < self.x.max + 2; ++i) {
+    for (size_t j = 0; j < self.y.max + 2; ++j) {
+      X_buf(i, j) = static_cast<double>(self.x[i]);
+      Y_buf(i, j) = static_cast<double>(self.y[j]);
+    }
+  }
+  return py::make_tuple(X, Y);
+}
+} // namespace
+
+namespace nsfdpy {
+namespace grid {
+
+void bindGrid(py::module_ &m) {
+  py::class_<nsfd::grid::Grid>(m, "Grid")
+      .def(py::init(&__init__), py::arg("geom_data"), py::arg("xfrac") = 1,
+           py::arg("yfrac") = 1)
+      .def_readonly("x", &nsfd::grid::Grid::x)
+      .def_readonly("y", &nsfd::grid::Grid::y)
+      .def("meshgrid", meshgrid);
+}
+} // namespace grid
+} // namespace nsfdpy
